806d8f6a16f40c914d9a35307f404c1935284db5
Danilo Lima <danilofive@gmail.com>
2017-02-23 00:00:22 -0300

    Update toggle_ec2_state.py

--- toggle_ec2_state.py
+++ toggle_ec2_state.py
@@ -1,21 +1,22 @@
-#!/dev/python
+#!/bin/python
 
 import boto3
 import os
+import sys
 
 # Environment variables to set
 # INSTANCE_ID=
-# INSTANCE_PUBLIC_IP=0.0.0.0
+# INSTANCE_PUBLIC_IP=
 # EIP_ALLOCATION_ID=
 # EIP_SCOPE=vpc|standard
+
 
 class ec2:
 
     instanceId = None
     instance = None
     ec2Client = None
-   
-    
+
     def __init__(self):
         self.instanceId = os.environ['INSTANCE_ID']
         self.ec2Client = boto3.resource("ec2")
@@ -25,7 +26,6 @@ class ec2:
     def getStatus(self):
         state = self.instance.state
         return state['Name']
-        
 
     def start(self):
         print "Starting instance ", self.instanceId
@@ -39,27 +39,31 @@ class ec2:
         self.instance.stop()
 
     def updateIpAddress(self):
-        
+
         scope = os.environ['EIP_SCOPE']
         client = boto3.client('ec2')
         public_ip = os.environ['INSTANCE_PUBLIC_IP']
         allocation_id = os.environ['EIP_ALLOCATION_ID']
 
-        if(scope == 'vpc'):
-            response = client.associate_address(
-                InstanceId = self.instanceId,
-                AllocationId=allocation_id
-            )
-        elif scope == 'standard':
-             response = client.associate_address(
-                InstanceId = self.instanceId,
-                PublicIp = public_ip
-            )
+        try:
+            if(scope == 'vpc'):
+                response = client.associate_address(
+                    InstanceId=self.instanceId,
+                    AllocationId=allocation_id
+                )
+            elif scope == 'standard':
+                response = client.associate_address(
+                    InstanceId=self.instanceId,
+                    PublicIp=public_ip
+                )
 
-        print "EC2 Public IP is ", public_ip
+            print "EC2 Public IP is ", public_ip
+        except:
+            print "Error allocating public ip address ", public_ip
+
 
     def toggleStatus(self):
-        status = str(self.getStatus());
+        status = str(self.getStatus())
 
         print "Instance status", status
         if status == "running":
@@ -68,10 +72,19 @@ class ec2:
             self.start()
 
 
-
 if __name__ == '__main__':
     ec2 = ec2()
-    ec2.toggleStatus()
-    
 
+    if len(sys.argv) == 2:
+        command = sys.argv[1]
+    else:
+        command = 'toggle'
 
+    if command == 'toggle':
+        ec2.toggleStatus()
+    elif command == 'status':
+        print ec2.getStatus()
+    elif command == 'start':
+        ec2.start()
+    elif command == 'stop':
+        ec2.stop()
